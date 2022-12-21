d677f4216496bd23e361974f6179afaf277d86f9
Myth0000 <infinitygovind@gmail.com>
2022-12-21 15:42:14 -0500

    Updated HTML

--- src/pages/view.js
+++ src/pages/view.js
@@ -1,7 +1,5 @@
 import React from "react";
 import HomePage from "./homepage";
-import AboutMe from "./aboutme";
-import websiteLogo from "../images/favicon.ico";
 
 export default class View extends React.Component {
 
@@ -11,17 +9,7 @@ export default class View extends React.Component {
         
         this.state = {
             view: <HomePage />,
-            navigationBarWidth: document.documentElement.clientWidth,
         }
-        /*
-        window.addEventListener("resize", () => {
-            this.setState({
-                navigationBarWidth: document.documentElement.clientWidth
-            })
-
-            console.log(`Width: ${this.navigationBarWidth}`);
-        })
-        */
     }
 
 
@@ -30,64 +18,6 @@ export default class View extends React.Component {
             <div>
                 {this.state.view}
             </div>
-
-
-
-
-
-
-
-
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        // navigation_bar, etc....
-        /*
-        <div>
-            <div
-                className="navigation_bar"
-                style={{ width: this.state.navigationBarWidth}}
-            >
-                <img
-                id="logo"
-                className="navigation_bar_child"
-                src={websiteLogo}
-                alt="Website Logo"
-                />
-                <p id="title" className="navigation_bar_child">
-                <b>Website Title</b>
-                </p>
-
-                <div className="navigation_bar_child navigation_buttons_container">
-                <button
-                    className="navigation_button"
-                    onClick={() => this.NavigationButtonClicked(<HomePage />)}
-                >
-                    Home
-                </button>
-                <button
-                    className="navigation_button"
-                    onClick={() => this.NavigationButtonClicked(<AboutMe />)}
-                >
-                    About Me
-                </button>
-                </div>
-            </div>
-        </div>
-        */
         );
     }
 }

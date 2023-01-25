752936d375a84e73b62a0587a8d87b07dd327187
Myth0000 <infinitygovind@gmail.com>
2023-01-25 14:47:10 -0500

    Fixed BUG in ProjectOverview's Slideshow

--- src/components/Slideshow.js
+++ src/components/Slideshow.js
@@ -2,100 +2,108 @@ import React from "react";
 import forwardArrow from "../images/forwardArrow.png";
 import backwardArrow from "../images/backwardArrow.png";
 
-export default class Slideshow extends React.Component
-{
-    constructor(props)
-    {
-        super(props);
-        this.state = {slideShowIndex: props.SlideshowIndex != undefined ? props.SlideshowIndex : 0};
+export default class Slideshow extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      slideShowIndex:
+        props.SlideshowIndex != undefined ? props.SlideshowIndex : 0,
+      images: this.props.Images,
+    };
+  }
+  componentDidMount() {
+    // rerenders the banner when resizing page
+    window.addEventListener("resize", () => {
+      this.forceUpdate();
+    });
+  }
+
+  componentDidUpdate() {
+    if (this.props.Images != this.state.images) {
+      this.setState({
+        slideShowIndex:
+          this.props.SlideshowIndex != undefined
+            ? this.props.SlideshowIndex
+            : 0,
+        images: this.props.Images,
+      });
     }
-    // !!! try to make it so it checks the old & new props.Images array and if they don't match then update them & update this.state's index
-    componentDidMount() {
-        // rerenders the banner when resizing page
-        window.addEventListener("resize", () => {
-            this.forceUpdate();
-        });
+  }
 
-        // if(this.props.SlideshowIndex) {
-        //     this.setState({slideShowIndex: this.props.SlideshowIndex});
-        // }
+  forwardButtonClicked() {
+    let newIndex = this.state.slideShowIndex + 1;
+    if (this.props.Images[newIndex] === undefined) {
+      this.setState({ slideShowIndex: 0 });
+    } else {
+      this.setState({ slideShowIndex: newIndex });
     }
+  }
 
-    forwardButtonClicked()
-    {
-        let newIndex = this.state.slideShowIndex+1;
-        if(this.props.Images[newIndex] === undefined)
-        {
-            this.setState({slideShowIndex: 0});
-        }
-        else
-        {
-            this.setState({slideShowIndex: newIndex});
-        }
+  backwardButtonClicked() {
+    let newIndex = this.state.slideShowIndex - 1;
+    if (this.props.Images[newIndex] === undefined) {
+      this.setState({ slideShowIndex: this.props.Images.length - 1 });
+    } else {
+      this.setState({ slideShowIndex: newIndex });
     }
+  }
 
-    backwardButtonClicked()
-    {
-        let newIndex = this.state.slideShowIndex-1;
-        if(this.props.Images[newIndex] === undefined)
-        {
-            this.setState({slideShowIndex: this.props.Images.length - 1});
-        }
-        else
-        {
-            this.setState({slideShowIndex: newIndex});
-        }
-        
-    }
+  getImageIndicators() {
+    return this.props.Images.map((image, index) => {
+      if (this.state.slideShowIndex === index) {
+        return <ImageIndicator key={index} onImage="true" />;
+      } else {
+        return <ImageIndicator key={index} />;
+      }
+    });
+  }
 
-    getImageIndicators()
-    {
-        return this.props.Images.map((image, index) => {
-            if(this.state.slideShowIndex === index) { return <ImageIndicator key={index} onImage="true" />; }
-            else { return <ImageIndicator key={index} />; }
-        });
-    }
+  render() {
+    return (
+      <div className="slideshow">
+        <img
+          className="slideshowImage"
+          src={this.props.Images[this.state.slideShowIndex]}
+        />
 
-    render()
-    {
-        return (
-            <div className="slideshow">
-                <img className="slideshowImage" src={this.props.Images[this.state.slideShowIndex]} />
+        <div className="imageNavigationButtons">
+          <img
+            className="imageNavigationButton"
+            src={forwardArrow}
+            alt="forward arrow"
+            draggable="false"
+            onClick={() => this.forwardButtonClicked()}
+          />
+          <img
+            className="imageNavigationButton"
+            src={backwardArrow}
+            alt="forward arrow"
+            draggable="false"
+            onClick={() => this.backwardButtonClicked()}
+          />
+        </div>
 
-                <div className="imageNavigationButtons">
-                    <img className="imageNavigationButton" src={forwardArrow} alt="forward arrow" draggable="false"
-                    onClick={() => this.forwardButtonClicked()} />
-                    <img className="imageNavigationButton" src={backwardArrow} alt="forward arrow" draggable="false"
-                    onClick={() => this.backwardButtonClicked()} />
-                </div>
-
-                <div className="slideshowImageIndicators">
-                    {this.getImageIndicators()}
-                </div>
-            </div>
-        );
-    }
+        <div className="slideshowImageIndicators">
+          {this.getImageIndicators()}
+        </div>
+      </div>
+    );
+  }
 }
 
-
 function ImageIndicator(props) {
+  let slideshowImageIndicatorClassName =
+    document.documentElement.clientWidth > 500
+      ? "slideshowImageIndicator"
+      : "slideshowImageIndicatorMobile";
+  let slideshowImageIndicatorOnImageClassName =
+    document.documentElement.clientWidth > 500
+      ? "slideshowImageIndicatorOnImage"
+      : "slideshowImageIndicatorOnImageMobile";
 
-    let slideshowImageIndicatorClassName = (document.documentElement.clientWidth > 500) ? "slideshowImageIndicator" : "slideshowImageIndicatorMobile";
-    let slideshowImageIndicatorOnImageClassName = (document.documentElement.clientWidth > 500) ? "slideshowImageIndicatorOnImage" : "slideshowImageIndicatorOnImageMobile";
-
-    if(props.onImage)
-    {
-        return (
-            <div className={slideshowImageIndicatorOnImageClassName}>
-            </div>
-        );
-    }
-    else
-    {
-        return (
-            <div className={slideshowImageIndicatorClassName}>
-            </div>
-        );
-    }
-    
+  if (props.onImage) {
+    return <div className={slideshowImageIndicatorOnImageClassName}></div>;
+  } else {
+    return <div className={slideshowImageIndicatorClassName}></div>;
+  }
 }
--- src/components/projects.js
+++ src/components/projects.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import {getElement} from "../functions/doc.js";
+import { getElement } from "../functions/doc.js";
 import mockData from "../mockData.json";
 import SocialMedia from "./SocialMedia";
 import Slideshow from "./Slideshow";
@@ -14,17 +14,16 @@ export default class Projects extends React.Component {
       projects: [],
       projectOverviewInfo: {
         images: [],
-        slideshowIndex: 0
-      }
+        slideshowIndex: 0,
+      },
     };
   }
 
   componentDidMount() {
-
-    // MOCK DATA 
+    // MOCK DATA
     this.setState({
-            projects: mockData,
-          });
+      projects: mockData,
+    });
 
     // USE THIS AFTER I FINNALLY HOST THE ProjectsAPI
     // let projectsJson = fetch("/projects")
@@ -37,56 +36,66 @@ export default class Projects extends React.Component {
   }
 
   projectClicked(name, imageUrl, description, socialMediaArray, imagesArray) {
-
     // switches ProjectOverview's display to grid/flex depending on screen size
     let projectOverview = getElement(".ProjectOverview");
-    if(document.documentElement.clientWidth <= 625) {
+    if (document.documentElement.clientWidth <= 625) {
       projectOverview.style.display = "flex";
-    } else { projectOverview.style.display = "grid"; }
-    
+    } else {
+      projectOverview.style.display = "grid";
+    }
+
     // fills ProjectOverview with new content
     this.setState({
       projectOverviewInfo: {
         images: imagesArray,
-        slideshowIndex: this.state.projectOverviewInfo.slideshowIndex
-      }
+        slideshowIndex: this.state.projectOverviewInfo.slideshowIndex,
+      },
     });
     getElement(".ProjectOverview .Project p").textContent = name;
     getElement(".ProjectOverview .Project img").src = imageUrl;
     getElement(".ProjectOverview .ProjectDetails p").textContent = description;
 
     // removes any pre-existing social medias from the .ProjectSocialMediaContainer
-    let socialMediaContainer = getElement(".ProjectOverview .ProjectSocialMediaContainer");
-    while(socialMediaContainer.firstChild) {
+    let socialMediaContainer = getElement(
+      ".ProjectOverview .ProjectSocialMediaContainer"
+    );
+    while (socialMediaContainer.firstChild) {
       socialMediaContainer.removeChild(socialMediaContainer.firstChild);
     }
 
     // adds the list of social media associated with the project to project overview page
-    for(let [index, socialMedia] of socialMediaArray.entries()) {
+    for (let [index, socialMedia] of socialMediaArray.entries()) {
       // creates a social media
       let _socialMedia = document.createElement("a");
       _socialMedia.className = "SocialMedia";
       _socialMedia.id = "SmallSocialMedia";
       _socialMedia.href = socialMedia.link;
       _socialMedia.target = "_blank";
-      getElement(".ProjectOverview .ProjectSocialMediaContainer").appendChild(_socialMedia);
+      getElement(".ProjectOverview .ProjectSocialMediaContainer").appendChild(
+        _socialMedia
+      );
 
       // creates image for social media
       let socialMediaImage = document.createElement("img");
       socialMediaImage.draggable = false;
       socialMediaImage.src = socialMedia.imageUrl;
       socialMediaImage.alt = "Social Media Image";
-      getElement(".ProjectOverview .ProjectSocialMediaContainer .SocialMedia", index).appendChild(socialMediaImage);
+      getElement(
+        ".ProjectOverview .ProjectSocialMediaContainer .SocialMedia",
+        index
+      ).appendChild(socialMediaImage);
 
       // creates socialMedia element's content
       let socialMediaContent = document.createElement("p");
       socialMediaContent.textContent = socialMedia.content;
-      getElement(".ProjectOverview .ProjectSocialMediaContainer .SocialMedia", index).appendChild(socialMediaContent);
-    }    
+      getElement(
+        ".ProjectOverview .ProjectSocialMediaContainer .SocialMedia",
+        index
+      ).appendChild(socialMediaContent);
+    }
   }
 
   render() {
-
     // converts json project data into a Project react component
     const data = this.state.projects.map((project) => (
       <Project
@@ -97,7 +106,15 @@ export default class Projects extends React.Component {
         link={project.link}
         socialMediaArray={project.socialMediaArray}
         images={this.state.projectOverviewInfo.images}
-        onClick={() => this.projectClicked(project.name, project.imageUrl, project.description, project.socialMediaArray, project.images)}
+        onClick={() =>
+          this.projectClicked(
+            project.name,
+            project.imageUrl,
+            project.description,
+            project.socialMediaArray,
+            project.images
+          )
+        }
       />
     ));
 
@@ -110,88 +127,112 @@ export default class Projects extends React.Component {
         </p>
         <div className="projectsContainer">
           {data.length === 0 ? null : data}
-          <ProjectOverview name={null} description={null} imageUrl={null} slideshowIndex={this.state.projectOverviewInfo.slideshowIndex}
-      socialMediaArray={null} images={this.state.projectOverviewInfo.images}/>
+          <ProjectOverview
+            name={null}
+            description={null}
+            imageUrl={null}
+            slideshowIndex={this.state.projectOverviewInfo.slideshowIndex}
+            socialMediaArray={null}
+            images={this.state.projectOverviewInfo.images}
+          />
         </div>
       </div>
     );
   }
 }
 
-
-
-
 function Project(props) {
   return (
     <>
-      { /* If isDynamic=true, SimpleProject will change on hover & show ProjectOverview on click */ }
-      <SimpleProject isDynamic={true} name={props.name} imageUrl={props.imageUrl}
-      projectClicked={() => props.onClick(props.name, props.imageUrl, props.description, props.socialMediaArray, props.images)}/>
+      {/* If isDynamic=true, SimpleProject will change on hover & show ProjectOverview on click */}
+      <SimpleProject
+        isDynamic={true}
+        name={props.name}
+        imageUrl={props.imageUrl}
+        projectClicked={() =>
+          props.onClick(
+            props.name,
+            props.imageUrl,
+            props.description,
+            props.socialMediaArray,
+            props.images
+          )
+        }
+      />
     </>
   );
 }
 
-
-
-
 function ProjectOverview(props) {
-
   useEffect(() => {
     // changes display to grid/flex based on screen size
     window.addEventListener("resize", () => {
       let projectOverview = getElement(".ProjectOverview");
 
-      if(projectOverview.style.display !== "none") {
+      if (projectOverview.style.display !== "none") {
         let width = document.documentElement.clientWidth;
-        
-        if(width <= 625) { projectOverview.style.display = "flex"; }
-        else { projectOverview.style.display = "grid"; }
+
+        if (width <= 625) {
+          projectOverview.style.display = "flex";
+        } else {
+          projectOverview.style.display = "grid";
+        }
       }
     });
   });
 
-  
   return (
     <div className="ProjectOverview">
       <div className="ProjectHeader">
         <SimpleProject name={props.name} imageUrl={props.imageUrl} />
-        <div className="ProjectSocialMediaContainer">  </div>
+        <div className="ProjectSocialMediaContainer"> </div>
       </div>
       <div id="line" />
       <div className="ProjectDetails">
-        <Slideshow Images={props.images} SlideshowIndex={props.slideshowIndex}/>
-        <h1>{props.slideshowIndex}</h1>
+        <Slideshow
+          Images={props.images}
+          SlideshowIndex={props.slideshowIndex}
+        />
         <p> </p>
       </div>
-      <button onClick={() => document.getElementsByClassName("ProjectOverview")[0].style.display = "none"}>X</button>
+      <button
+        onClick={() =>
+          (document.getElementsByClassName("ProjectOverview")[0].style.display =
+            "none")
+        }
+      >
+        X
+      </button>
     </div>
   );
 }
 
-function SimpleProject(props)
-{
+function SimpleProject(props) {
   const [textDeco, setTextDecoration] = useState("none");
-  let content = <>
-    <img src={props.imageUrl} draggable="false"/>
-    <p style={{textDecoration: textDeco}}>{props.name}</p>
-  </>
-  
-  if(props.isDynamic === true)
-  {
+  let content = (
+    <>
+      <img src={props.imageUrl} draggable="false" />
+      <p style={{ textDecoration: textDeco }}>{props.name}</p>
+    </>
+  );
+
+  if (props.isDynamic === true) {
     return (
-      <div className="Project"
-      onMouseOver={() => { setTextDecoration("underline")}}
-      onMouseLeave={() => { setTextDecoration("none")}}
-      onClick={props.projectClicked}> {content} </div>
-    );
-  }
-  else
-  {
-    return (
-      <div className="Project">
-          {content}
+      <div
+        className="Project"
+        onMouseOver={() => {
+          setTextDecoration("underline");
+        }}
+        onMouseLeave={() => {
+          setTextDecoration("none");
+        }}
+        onClick={props.projectClicked}
+      >
+        {" "}
+        {content}{" "}
       </div>
-    )
+    );
+  } else {
+    return <div className="Project">{content}</div>;
   }
-  
 }
